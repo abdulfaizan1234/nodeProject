98b00bfe3e9581d71fd8b70da6105f7fc487f2cd
abdulfaizan1234 <afaizan@sunbonn.com>
2022-09-19 15:52:47 +0500

    done

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require("../server");
 
 describe("GET /", () => {
   it("respond with Hello World", (done) => {
-    request(app).get("/").expect("Hello World!", done);
+    request(app).get("/").expect("Hello World test!", done);
   })
 });
