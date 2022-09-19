8872bf2563deee8bb0469c3886aa6524a85c1862
abdulfaizan1234 <afaizan@sunbonn.com>
2022-09-19 18:19:58 +0500

    done

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ process.env.port = 3000
 const app = require("../server");
 describe("GET /", () => {
   it("respond with Hello World", (done) => {
-    request(app).get("/").expect("Hello World test123456!", done);
+    request(app).get("/").expect("Hello World test1234567!", done);
   })
 });
